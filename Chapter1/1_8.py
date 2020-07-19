bead094c6e61a029ba1c7f55dc113da482ae65b7
ChingChieh <garnett.huang@gmail.com>
2020-07-19 18:23:02 +0800

    Clean version using first row and col to store zeros' index

--- Chapter1/1_8.py
+++ Chapter1/1_8.py
@@ -3,33 +3,45 @@ def printMatrix(array):
     for i in range(length):
         print(array[i])
     print("")
+def nullifyRow(matrix, row, col):
+    for i in range(col):
+        matrix[row][i] = 0
+def nullifyCol(matrix, row, col):
+    for i in range(row):
+        matrix[i][col] = 0
+
 def setZero(matrix):
-    # Use the first row we spot that contain zero as buffer
-    counter = -1
+    # time complexity: O(N^2)
+    # Use the first column and first row to store zero index
     row = len(matrix)
     col = len(matrix[0])
-    for i in range(row):
-        for j in range(col):
+    rowHasZero = False
+    colHasZero = False
+    for i in range(col):
+        if matrix[0][i] == 0:
+            rowHasZero = True
+            break
+    for j in range(row):
+        if matrix[j][0] == 0:
+            colHasZero = True
+            break
+
+    for i in range(1, row):
+        for j in range(1, col):
             if matrix[i][j] == 0:
-                if counter < 0:
-                    counter = i
-                matrix[counter][j] = 0
+                matrix[0][j] = 0
+                matrix[i][0] = 0
 
-    for i in range(row):
-        for j in range(col):
-            if i == counter:
-                break;
-            if matrix[i][j] == 0:
-                matrix[i] = [0] * row
-                break;
-
-    if counter != -1:
-        for l in range(col):
-            if matrix[counter][l] == 0:
-                for m in range(row):
-                    matrix[m][l] = 0
-        matrix[counter] = [0] * col
-
+    for i in range(1, row):
+        if matrix[i][0] == 0:
+            nullifyRow(matrix, i, col)
+    for i in range(1, col):
+        if matrix[0][i] == 0:
+            nullifyCol(matrix, row, i)
+    if rowHasZero:
+        nullifyRow(matrix, 0, col)
+    if colHasZero:
+        nullifyCol(matrix, row, 0)
 if __name__ == "__main__":
     matrix = [
     [0, 1, 1, 1, 1, 1],
