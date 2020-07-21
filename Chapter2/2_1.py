61eeafa8933cb42b746622721105cdbf5e782778
ChingChieh <garnett.huang@gmail.com>
2020-07-21 11:32:45 +0800

    Use node.next as iterator

--- Chapter2/2_1.py
+++ Chapter2/2_1.py
@@ -1,68 +1,29 @@
-class ListNode:
-    def __init__(self, value = None):
-        self.data = value
-        self.next = None
-        return
+from LinkedList import LinkedListNode as Node
+from LinkedList import LinkedList as linkedlist
 
-class SingleLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        return
+def delDuplicate_dict(llist):
+    node = llist.head
+    dictionary = {str(node):node}
+    while node.next:
+        if str(node.next) not in dictionary:
+            dictionary[str(node.next)] = node.next
+            node = node.next
+        else:
+            node.next = node.next.next
 
-    def add_list_item(self, node):
-        if self.head == None : self.head = node
-        else : self.tail.next = node
-        self.tail = node
-        return 
+def delDuplicate_set(llist):
+    node = llist.head
+    seen = set([node.value])
+    while node.next:
+        if node.next.value in seen:
+            node.next = node.next.next
+        else:
+            seen.add(node.next.value)
+            node = node.next
 
-    def list_length(self):
-        count = 0
-        current_node = self.head
-        while current_node : 
-            count += 1
-            current_node =  current_node.next
-        return count
-    
-    def print_list(self):
-        current_node = self.head
-        while current_node :
-            print(str(current_node.data) + " -> ", end = '')
-            current_node = current_node.next
-        print()
-        return 
-
-def deleteDup(head):
-    HastTable = set()
-    previous = ListNode()
-    while head :
-        if not (head.data in HastTable) : 
-            HastTable.add(head.data)
-            previous = head
-        else : 
-            previous.next = head.next
-        head = head.next
-        print(HastTable)
-    return head
 
 if __name__ == "__main__":
-    node = [ListNode() for i in range(20)]
-    # node = []
-    # for i in range(20):
-    #     node.append(ListNode(i))
-    Slist = SingleLinkedList()
-    
-    for i in range(0, 20):
-        node[i].data = i
-        Slist.add_list_item(node[i])
-
-    node[4].data = 9
-    node[7].data = 13
-    node[3].data = 5
-
-    Slist.print_list()
-    deleteDup(Slist.head)
-    Slist.print_list()
-    #Slist.add_list_item(node)
-    #Slist.add_list_item(node)
-    #print(Slist.list_length())
+    llist = linkedlist().generate(30, 10, 0)
+    print(llist)
+    delDuplicate_set(llist)
+    print(llist)
